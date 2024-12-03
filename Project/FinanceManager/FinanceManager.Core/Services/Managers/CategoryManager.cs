675bbaa1c52aeb953ed6f70b40cb41b422d4a5ca
proninp <proninpaul@yandex.ru>
2024-12-03 19:54:58 +0300

    Separate CategoryManager from BaseManager

--- Project/FinanceManager/FinanceManager.Core/Services/Managers/CategoryManager.cs
+++ Project/FinanceManager/FinanceManager.Core/Services/Managers/CategoryManager.cs
@@ -5,12 +5,18 @@ using FinanceManager.Core.Models;
 using FinanceManager.Core.Services.Abstractions;
 using FinanceManager.Core.Services.Abstractions.Managers;
 using FinanceManager.Core.Services.Abstractions.Repositories;
+using FinanceManager.Core.Services.Interfaces.Managers;
 
 namespace FinanceManager.Core.Services.Managers;
-public class CategoryManager : BaseManager<Category, CategoryDto, CreateCategoryDto, UpdateCategoryDto>, ICategoryManager
+public sealed class CategoryManager : ICategoryManager, IEntityProvider<Category>
 {
-    public CategoryManager(IRepository<Category> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+    private readonly IRepository<Category> _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryManager(IRepository<Category> repository, IUnitOfWork unitOfWork)
     {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<CategoryDto?> GetById(Guid id)
@@ -18,18 +24,36 @@ public class CategoryManager : BaseManager<Category, CategoryDto, CreateCategory
         return (await _repository.GetById(id))?.ToDto();
     }
 
-    // TODO Избавиться от async await там, где они не нужны
     public Task<CategoryDto[]> Get(Guid userId)
     {
         return _repository.Get(c => c.UserId == userId, c => c.ToDto());
     }
 
-    protected override void UpdateModel(Category category, UpdateCategoryDto command)
+    public async Task<CategoryDto> Create(CreateCategoryDto command)
     {
-        category.Title = command.Title;
-        category.ParentCategoryId = command.ParentCategoryId;
+        var model = _repository.Add(command.ToModel());
+        await _unitOfWork.Commit();
+        return model.ToDto();
     }
 
-    protected override CategoryDto GetViewDto(Category model) =>
-        model.ToDto();
+    public async Task<CategoryDto> Update(UpdateCategoryDto command)
+    {
+        var entityProvider = (IEntityProvider<Category>)this; // TODO Questionable: IEntityProvider
+        var category = await entityProvider.GetEntityById(_repository, command.Id);
+
+        category.Title = command.Title;
+        category.ParentCategoryId = command.ParentCategoryId;
+
+        _repository.Update(category);
+        await _unitOfWork.Commit();
+        return category.ToDto();
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var entityProvider = (IEntityProvider<Category>)this; // TODO Questionable: IEntityProvider
+        var category = await entityProvider.GetEntityById(_repository, id);
+        _repository.Delete(category);
+        await _unitOfWork.Commit();
+    }
 }
