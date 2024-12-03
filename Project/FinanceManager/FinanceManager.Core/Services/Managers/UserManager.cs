f2ccc8aeb31ac98718bab3c3a84e58a6384ef943
proninp <proninpaul@yandex.ru>
2024-12-03 22:16:24 +0300

    Separate UserManager from BaseManager

--- Project/FinanceManager/FinanceManager.Core/Services/Managers/UserManager.cs
+++ Project/FinanceManager/FinanceManager.Core/Services/Managers/UserManager.cs
@@ -5,13 +5,19 @@ using FinanceManager.Core.Models;
 using FinanceManager.Core.Services.Abstractions;
 using FinanceManager.Core.Services.Abstractions.Managers;
 using FinanceManager.Core.Services.Abstractions.Repositories;
+using FinanceManager.Core.Services.Interfaces.Managers;
 
 namespace FinanceManager.Core.Services.Managers;
 
-public class UserManager : BaseManager<User, UserDto, CreateUserDto, UpdateUserDto>, IUserManager
+public sealed class UserManager : IUserManager, IEntityProvider<User>
 {
-    public UserManager(IRepository<User> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+    private readonly IRepository<User> _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UserManager(IRepository<User> repository, IUnitOfWork unitOfWork)
     {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<UserDto?> GetById(Guid id)
@@ -19,11 +25,30 @@ public class UserManager : BaseManager<User, UserDto, CreateUserDto, UpdateUserD
         return (await _repository.GetById(id))?.ToDto();
     }
 
-    protected override UserDto GetViewDto(User model) =>
-        model.ToDto();
-
-    protected override void UpdateModel(User user, UpdateUserDto command)
+    public async Task<UserDto> Create(CreateUserDto command)
     {
+        var user = _repository.Add(command.ToModel());
+        await _unitOfWork.Commit();
+        return user.ToDto();
+    }
+
+    public async Task<UserDto> Update(UpdateUserDto command)
+    {
+        var entityProvider = (IEntityProvider<User>)this; // TODO Questionable: IEntityProvider
+        var user = await entityProvider.GetEntityById(_repository, command.Id);
+
         user.Name = command.Name;
+
+        _repository.Update(user);
+        await _unitOfWork.Commit();
+        return user.ToDto();
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var entityProvider = (IEntityProvider<User>)this; // TODO Questionable: IEntityProvider
+        var user = await entityProvider.GetEntityById(_repository, id);
+        _repository.Delete(user);
+        await _unitOfWork.Commit();
     }
 }
