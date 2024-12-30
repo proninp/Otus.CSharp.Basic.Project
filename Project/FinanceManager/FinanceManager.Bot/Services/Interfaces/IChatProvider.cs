99e85a6da8a723bb8e94be0c6117f43caa951b86
proninp <proninpaul@yandex.ru>
2024-12-30 19:07:24 +0300

    Added ChatProvider and UpdateValidator

---
+++ Project/FinanceManager/FinanceManager.Bot/Services/Interfaces/IChatProvider.cs
@@ -0,0 +1,7 @@
+﻿using Telegram.Bot.Types;
+
+namespace FinanceManager.Bot.Services.Interfaces;
+public interface IChatProvider
+{
+    Chat GetChat(Update update);
+}
---
+++ Project/FinanceManager/FinanceManager.Bot/Services/Telegram/UpdateValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FinanceManager.Bot.Services.Interfaces;
+using Serilog;
+using Telegram.Bot.Types;
+
+namespace FinanceManager.Bot.Services.Telegram;
+public class UpdateValidator : IUpdateValidator
+{
+    private readonly ILogger _logger;
+
+    public UpdateValidator(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public bool Validate(Update update, [NotNullWhen(true)] out User? user)
+    {
+        user = update switch
+        {
+            { Message: { Text: var text, From: var from } } when !string.IsNullOrEmpty(text) => from,
+            { CallbackQuery: { Data: var data, From: var from } } when !string.IsNullOrEmpty(data) => from,
+            _ => null
+        };
+
+        if (user is null)
+            UnknownTypeUpdate(update);
+
+        return user is not null;
+    }
+
+    private bool UnknownTypeUpdate(Update update)
+    {
+        _logger.Information("Unknown update type: {UpdateType}", update.Type);
+        return false;
+    }
+}
