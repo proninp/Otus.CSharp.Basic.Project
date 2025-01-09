cc43eabe46c9ddddb6a1ba033c1addd54c6bcde0
proninp <proninpaul@yandex.ru>
2025-01-09 09:57:40 +0300

    Additional date parsing logic in transaction date provider

--- Project/FinanceManager/FinanceManager.Bot/Services/Interfaces/Providers/ITransactionDateProvider.cs
+++ Project/FinanceManager/FinanceManager.Bot/Services/Interfaces/Providers/ITransactionDateProvider.cs
@@ -4,6 +4,4 @@ public interface ITransactionDateProvider
     bool TryParseDate(string? input, out DateOnly date);
 
     string GetIncorrectDateText();
-
-    string GetSupportedFormatsText();
 }
--- Project/FinanceManager/FinanceManager.Bot/Services/Utils/TransactionDateProvider.cs
+++ Project/FinanceManager/FinanceManager.Bot/Services/Utils/TransactionDateProvider.cs
@@ -6,28 +6,7 @@ using FinanceManager.Bot.Services.Interfaces.Providers;
 namespace FinanceManager.Bot.Services.Utils;
 public class TransactionDateProvider : ITransactionDateProvider
 {
-    public string GetIncorrectDateText()
-    {
-        var formats = string.Join(Environment.NewLine, GetDateFormats());
-
-        var messageBuilder = new StringBuilder($"{Emoji.Error.GetSymbol()} ");
-        messageBuilder
-            .AppendLine($"**An incorrect date has been entered.**")
-            .AppendLine(GetSupportedFormatsText())
-            .AppendLine("You can enter **'T'** for today's date")
-            .Append("or **'Y'** for yesterday's date.");
-
-        return messageBuilder.ToString();
-    }
-
-    public string GetSupportedFormatsText()
-    {
-        var formats = string.Join(Environment.NewLine, GetDateFormats());
-        var messageBuilder = new StringBuilder();
-        messageBuilder.AppendLine("Supported formats:");
-        messageBuilder.Append(formats);
-        return messageBuilder.ToString();
-    }
+    private static readonly char[] _separators = ['-', ' ', '.', '/'];
 
     public bool TryParseDate(string? input, out DateOnly date)
     {
@@ -47,12 +26,31 @@ public class TransactionDateProvider : ITransactionDateProvider
             return true;
         }
 
+        if (GetDateFromDateParts(input, ref date))
+            return true;
+
         string[] formats = GetDateFormats();
 
         foreach (var format in formats)
             if (DateOnly.TryParseExact(input, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
                 return true;
         return false;
+    }
+
+    public string GetIncorrectDateText()
+    {
+        var delimitersText = string.Join(", ", _separators.Select(c => $"<code>{c}</code>"));
+
+        var messageBuilder = new StringBuilder($"{Emoji.Error.GetSymbol()} ");
+        messageBuilder
+            .AppendLine($"<b>An incorrect date has been entered.</b>")
+            .AppendLine("You can use the following delimiters to enter the date:")
+            .AppendLine(delimitersText)
+            .AppendLine()
+            .AppendLine("You can either enter <code>T</code> for today's date or <code>Y</code> for yesterday's date.")
+            .Append("Alternatively, you can specify only the day or the day and month.");
+
+        return messageBuilder.ToString();
     }
 
     private string[] GetDateFormats() =>
@@ -62,4 +60,36 @@ public class TransactionDateProvider : ITransactionDateProvider
             "d.MM.yy", "dd.MM.yyyy", "dd.MM.yy", "dd MM yyyy", "dd MM yy",
             "ddMMyyyy", "ddMMyy"
         ];
+
+    private bool GetDateFromDateParts(string input, ref DateOnly date)
+    {
+        var parts = input.Split(_separators, StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length == 0 || parts.Length > 2)
+            return false;
+        if (!int.TryParse(parts[0], out var day))
+            return false;
+
+        var month = DateTime.Now.Month;
+
+        if (parts.Length > 1)
+            if (!int.TryParse(parts[1], out month))
+                return false;
+
+        if (!CheckValidDateParts(day, month))
+            return false;
+
+        var currentYear = DateTime.Now.Year;
+        date = DateOnly.FromDateTime(new DateTime(currentYear, month, day));
+        return true;
+    }
+
+    private bool CheckValidDateParts(int day, int month)
+    {
+        if (month < 1 ||  month > 12)
+            return false;
+        int daysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, month);
+        if (day < 1 || day > daysInMonth)
+            return false;
+        return true;
+    }
 }
