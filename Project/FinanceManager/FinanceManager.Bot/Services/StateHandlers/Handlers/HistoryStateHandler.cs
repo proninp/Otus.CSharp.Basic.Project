f8d2f0466bb9a8d38a9abb9308af639e7dda03ad
proninp <proninpaul@yandex.ru>
2025-01-11 08:47:24 +0300

    Added logic to History state handler and Setting state handler

--- Project/FinanceManager/FinanceManager.Bot/Services/StateHandlers/Handlers/HistoryStateHandler.cs
+++ Project/FinanceManager/FinanceManager.Bot/Services/StateHandlers/Handlers/HistoryStateHandler.cs
@@ -1,4 +1,8 @@
-﻿using FinanceManager.Bot.Models;
+﻿using FinanceManager.Application.Services.Interfaces.Managers;
+using FinanceManager.Bot.Enums;
+using FinanceManager.Bot.Models;
+using FinanceManager.Bot.Services.Interfaces.Managers;
+using FinanceManager.Bot.Services.Interfaces.Providers;
 using FinanceManager.Bot.Services.Interfaces.StateHandlers;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -6,9 +10,54 @@ using Telegram.Bot.Types;
 namespace FinanceManager.Bot.Services.CommandHandlers.Handlers;
 public class HistoryStateHandler : IStateHandler
 {
-    public Task HandleStateAsync(
+    private readonly IChatProvider _chatProvider;
+    private readonly IUpdateCallbackQueryProvider _callbackQueryProvider;
+    private readonly IMessageSenderManager _messageSender;
+    private readonly ITransactionManager _transactionManager;
+    private readonly IAccountManager _accountManager;
+
+    public HistoryStateHandler(
+        IChatProvider chatProvider,
+        IUpdateCallbackQueryProvider callbackQueryProvider,
+        IMessageSenderManager messageSender,
+        ITransactionManager transactionManager,
+        IAccountManager accountManager)
+    {
+        _chatProvider = chatProvider;
+        _callbackQueryProvider = callbackQueryProvider;
+        _messageSender = messageSender;
+        _transactionManager = transactionManager;
+        _accountManager = accountManager;
+    }
+
+    public async Task HandleStateAsync(
         UserSession session, ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
+        if (!_chatProvider.GetChat(update, out var chat))
+            return;
+
+        var account = await _accountManager.GetDefault(session.Id, cancellationToken);
+        if (account is null)
+        {
+            var message = "The operation cannot be performed because you do not have a default account." +
+                "Please create a default account first.";
+            await _messageSender.SendErrorMessage(botClient, chat, message, cancellationToken);
+            session.Continue(WorkflowState.Default);
+            return;
+        }
+
+        var transactionsCount = await _transactionManager.GetCount(session.Id, account.Id, cancellationToken);
+        if (transactionsCount == 0)
+        {
+            await _messageSender.SendMessage(
+                botClient, chat,
+                "At the moment, you do not have any registered transactions on the selected account.",
+                cancellationToken);
+            session.Continue(WorkflowState.Default);
+            return;
+        }
+
+
         throw new NotImplementedException();
     }
 
--- Project/FinanceManager/FinanceManager.Bot/Services/StateHandlers/Handlers/SettingsStateHandler.cs
+++ Project/FinanceManager/FinanceManager.Bot/Services/StateHandlers/Handlers/SettingsStateHandler.cs
@@ -1,5 +1,7 @@
 ﻿using FinanceManager.Bot.Enums;
 using FinanceManager.Bot.Models;
+using FinanceManager.Bot.Services.Interfaces.Managers;
+using FinanceManager.Bot.Services.Interfaces.Providers;
 using FinanceManager.Bot.Services.Interfaces.StateHandlers;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -7,10 +9,24 @@ using Telegram.Bot.Types;
 namespace FinanceManager.Bot.Services.CommandHandlers.Handlers;
 public class SettingsStateHandler : IStateHandler
 {
-    public Task HandleStateAsync(
+    private readonly IChatProvider _chatProvider;
+    private readonly IMessageSenderManager _messageSender;
+
+    public SettingsStateHandler(IChatProvider chatProvider, IMessageSenderManager messageSender)
+    {
+        _chatProvider = chatProvider;
+        _messageSender = messageSender;
+    }
+
+    public async Task HandleStateAsync(
         UserSession session, ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (!_chatProvider.GetChat(update, out var chat))
+            return;
+
+        await _messageSender.SendMessage(
+            botClient, chat, $"The settings feature is under development {Emoji.Rocket.GetSymbol()}", cancellationToken);
+        session.Continue(WorkflowState.Menu);
     }
 
     public Task RollBackAsync(UserSession session, CancellationToken cancellationToken)
