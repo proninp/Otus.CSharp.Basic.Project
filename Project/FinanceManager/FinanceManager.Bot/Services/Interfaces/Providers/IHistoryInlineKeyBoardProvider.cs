7a2f8b4967f8ab4d0e2970db845ebafad84265f6
proninp <proninpaul@yandex.ru>
2025-01-18 11:16:46 +0300

    Added history Inline keyboard provider

---
+++ Project/FinanceManager/FinanceManager.Bot/Services/Interfaces/Providers/IHistoryInlineKeyBoardProvider.cs
@@ -0,0 +1,8 @@
+﻿using FinanceManager.Bot.Models;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace FinanceManager.Bot.Services.Interfaces.Providers;
+public interface IHistoryInlineKeyBoardProvider
+{
+    InlineKeyboardMarkup GetKeyboard(BotUpdateContext updateContext);
+}
---
+++ Project/FinanceManager/FinanceManager.Bot/Services/Telegram/Providers/HistoryInlineKeyBoardProvider.cs
@@ -0,0 +1,52 @@
+﻿using FinanceManager.Application.Utils;
+using FinanceManager.Bot.Enums;
+using FinanceManager.Bot.Models;
+using FinanceManager.Bot.Services.Interfaces.Providers;
+using FinanceManager.Bot.Services.StateHandlers.Contexts;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace FinanceManager.Bot.Services.Telegram.Providers;
+public class HistoryInlineKeyBoardProvider : IHistoryInlineKeyBoardProvider
+{
+    public InlineKeyboardMarkup GetKeyboard(BotUpdateContext updateContext)
+    {
+        var next = HistoryCommand.Next;
+        var previous = HistoryCommand.Previous;
+        var menu = HistoryCommand.Memu;
+
+        var context = updateContext.Session.GetHistoryContext();
+
+        var isNextAvailable = context.PageIndex < context.TransactionsCount / context.PageSize;
+        var isPreviousAvailable = context.PageIndex > 0;
+
+        var backButton = InlineKeyboardButton.WithCallbackData(
+                    $"{menu.GetSymbol()} {menu.GetDescription()}", menu.ToString());
+
+        if (!(isNextAvailable || isPreviousAvailable))
+            return new InlineKeyboardMarkup(backButton);
+
+        var controlButtons = new List<InlineKeyboardButton>();
+
+        if (isNextAvailable)
+        {
+            controlButtons.Add(
+                InlineKeyboardButton.WithCallbackData(
+                    $"{next.GetSymbol()} {next.GetDescription()}", next.ToString()));
+        }
+
+        if (isPreviousAvailable)
+        {
+            controlButtons.Add(
+                InlineKeyboardButton.WithCallbackData(
+                    $"{previous.GetSymbol()} {previous.GetDescription()}", previous.ToString()));
+        }
+
+        var buttons = new InlineKeyboardButton[][]
+        {
+            controlButtons.ToArray(),
+            [backButton]
+        };
+
+        return new InlineKeyboardMarkup(buttons);
+    }
+}
