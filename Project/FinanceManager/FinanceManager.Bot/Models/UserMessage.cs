d18b60d92b317986099825bf100fd1604e6b32f4
proninp <proninpaul@yandex.ru>
2025-01-28 10:03:34 +0300

    Added UserMessageModel

---
+++ Project/FinanceManager/FinanceManager.Bot/Models/UserMessage.cs
@@ -0,0 +1,42 @@
+﻿using Telegram.Bot.Types;
+
+namespace FinanceManager.Bot.Models;
+public class UserMessage
+{
+    public int Id { get; set; }
+
+    public long FromId { get; set; }
+
+    public long ChatId { get; set; }
+
+    public string? Text { get; set; }
+
+    public bool IsContainsInlineKeyboard { get; set; }
+
+    public UserMessage() { }
+
+    public UserMessage(int id, long fromId, long chatId, string? text = null, bool isContainsInlineKeyboard = false)
+    {
+        Id = id;
+        FromId = fromId;
+        ChatId = chatId;
+        Text = text;
+        IsContainsInlineKeyboard = isContainsInlineKeyboard;
+    }
+}
+
+public static class MessageExtension
+{
+    public static UserMessage ToUserMessage(this Message message)
+    {
+        ArgumentNullException.ThrowIfNull(message.From, nameof(message.From));
+
+        return new UserMessage(
+            message.Id,
+            message.From.Id,
+            message.Chat.Id,
+            message.Text,
+            message.ReplyMarkup?.InlineKeyboard is not null
+        );
+    }
+}
